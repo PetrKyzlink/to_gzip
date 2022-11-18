633c8f3181bdf89d47849c597f3a334b5cebc356
Petr Kyzlink <112183419+PetrKyzlink@users.noreply.github.com>
2022-11-18 18:38:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import shutil
 import os
 
 # Připojení umístění souborů
-read_folder = os.path.join("var", "log", "")
+read_folder = os.getcwd()
+read_folder = os.path.join("var", "log", "")  # ("..", "log", "")
 
 # Procházení souborů ve složce umístění cyklem for
 for file_input in os.listdir(read_folder):
-    if not file_input.endswith(".gz"):   # Podmínka pro vynechání již
-                                         # zabalených souborů
+    if not file_input.endswith(".gz") and not file_input.endswith(".py"):   # Podmínka pro vynechání již
+                                                                            # zabalených souborů
         with open(read_folder + file_input, mode="rb") as f_in:
             with gzip.open(read_folder + file_input + ".gz", mode='wb',
                            compresslevel=9) as f_out:
